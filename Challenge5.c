355a1dcd84c19603f8a25f53d6a4c9429445b127
Deniz <thetoasterofdoom1@gmail.com>
2019-02-06 16:03:27 -0500

    This is what we have after class

--- Challenge5.c
+++ Challenge5.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+// problem 1
 struct Cred{
   char username;
   struct *Cred next;
 };
+
+// problem 2
+/*
+ The first argument is the username, the second is the password, and the third is a pointer to another struct Cred.
+*/
+
+struct Cred* Cred(char username[], char password[], struct Cred *link) {
+
+}
 
 
 
