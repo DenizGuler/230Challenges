5688e35fbbf667514a1e834e009457790370a3a5
Deniz <thetoasterofdoom1@gmail.com>
2019-02-06 15:44:47 -0500

    initial commit

---
+++ Challenge5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct Cred{
+  char username;
+  struct *Cred next;
+};
+
+
+
+// See the README.txt file for the challenge description.
+int main(void) {
+  printf("Get to work!\n");
+  return 0;
+}
