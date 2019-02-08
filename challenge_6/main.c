f7dbc995da87ba029f8f51f08a464ced8cf3d768
Deniz <thetoasterofdoom1@gmail.com>
2019-02-08 15:47:11 -0500

    almost done\n

--- challenge_6/main.c
+++ challenge_6/main.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main()
+#define FILE_PATH "script.txt"
+#define CONTINUE lineCount < 20 && feof(file) == false
+
+int main(void){
+    FILE *file = fopen(FILE_PATH, "r");
+
+    int lineCount = 0;
+    char currentChar = '\0', input;
+	lable1:
+    do {
+        currentChar = getc(file);
+        if(currentChar == '\n'){
+            ++lineCount;
+        }
+		printf("%c", currentChar);
+
+	} while (CONTINUE);
+	scanf("%s", &input);
+	if (input != 'q')
+		goto lable1;
+	return 0;
+}
