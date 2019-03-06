347f9eecf30218407c2d2d07f1a528fe66cd5ee6
Deniz <thetoasterofdoom1@gmail.com>
2019-03-06 09:49:24 -0500

    this is the merge

--- challenge_6/main.c
+++ challenge_6/main.c
@@ -5,27 +5,21 @@
 #define CONTINUE line_count < 20 && !feof(file)// || getchar() == 'q'
 
 int main(void){
-    FILE *file = fopen(FILE_PATH, "r");
+	FILE *file = fopen(FILE_PATH, "r");
 
-<<<<<<< HEAD
-    int line_count;
-    char current_char = '\0', input;
-=======
-    int lineCount = 0;
-    int currentChar;
-    char input;
->>>>>>> 2619ef305acd88dc0489325508c5c59bd635fcf3
-	lable1:
+	int line_count;
+	char current_char = '\0', input;
+lable1:
 	line_count = 0;
-    do {
-        current_char = getc(file);
-        if(current_char == '\n'){
-            ++line_count;
-        }
+	do {
+		current_char = getc(file);
+		if(current_char == '\n') {
+			++line_count;
+		}
 		printf("%c", current_char);
 	} while (CONTINUE);
-	if (getchar() != 'q'){
-	   goto lable1;
-   }
+	if (getchar() != 'q') {
+		goto lable1;
+	}
 	return 0;
 }
