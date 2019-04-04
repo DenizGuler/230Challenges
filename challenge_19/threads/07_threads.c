701087a575670d0c871788ab4379eab9dce5f8c2
vsoltan <vsoltan@umass.edu>
2019-04-04 02:48:23 +0000

    joined threads instead of detaching to avoid race condition

--- challenge_19/threads/07_threads.c
+++ challenge_19/threads/07_threads.c
@@ -8,20 +8,16 @@ void *thread(void *vargp);
 int main(int argc, char **argv) {
   pthread_t tid;
   int i;
-  for (i = 0; i < 100; i++) {
+  for (i = 0; i < 100; ++i) {
     pthread_create(&tid, NULL, thread, (void *)&i);
+    // instead of detaching, wait for the thread to
+    // complete before creating a new one
+    pthread_join(tid, NULL);
   }
 }
 
 void *thread(void *vargp) {
-  // Does this code create a "race condition"?
-  // Race Test:
-  //  (1) If there is no race, then each thread would get a different
-  //      value of i (received from the main thread).
-  //  (2) If there is no race, the set of values printed to stdout
-  //      would consist of one output for each value from 0 to 99.
   int i = *((int *)vargp);
-  pthread_detach(pthread_self());
   printf("%d\n", i);
   return NULL;
 }
