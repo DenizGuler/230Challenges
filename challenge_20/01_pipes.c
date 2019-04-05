a24faad1f085bb8ddfb901004c1bb464770f0211
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-04-05 17:06:50 -0400

    implemented solution for part 1

--- challenge_20/01_pipes.c
+++ challenge_20/01_pipes.c
@@ -5,9 +5,14 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int child_task(int read_pipe) {
+#define READ 0
+#define WRITE 1
+#define BUFFER_SIZE 25
+
+int child_task(int read_pipe, int write_pipe) {
   char to_msg[25];
-  int nr;
+  char from_msg[] = "hello from child";
+  int nr, nw;
 
   if ((nr = read(read_pipe, to_msg, 25)) == -1) {
     perror("child failed to read from pipe");
@@ -18,14 +23,26 @@ int child_task(int read_pipe) {
   printf("bytes: %d\n", nr);
   printf("message received: %s\n", to_msg);
 
+  nw = write(write_pipe, from_msg, strlen(from_msg)) + 1;
+
+  if (nw == -1) {
+      perror("write");
+      return 1;
+  }
+
   return 0;
 }
 
-int parent_task(int write_pipe) {
-  int nw;
+int parent_task(int read_pipe, int write_pipe) {
+
+  int nw, nr;
+  char to_msg[25];
   char from_msg[] = "hello from parent.";
 
-  if ((nw = write(write_pipe, from_msg, strlen(from_msg) + 1)) == -1) {
+
+  nw = write(write_pipe, from_msg, strlen(from_msg) + 1);
+
+  if (nw == -1) {
     perror("parent failed to write to pipe");
     exit(1);
   }
@@ -34,14 +51,33 @@ int parent_task(int write_pipe) {
   printf("message sent: %s.\n", from_msg);
   printf("bytes written: %d\n", nw);
 
+  wait(NULL);
+
+  if ((nr = read(read_pipe, to_msg, 25)) == -1) {
+    perror("child failed to read from pipe");
+    return 1;
+  }
+
+  to_msg[nr] = '\0';
+
+  printf("parent recieved message from child.\n");
+  printf("message recieved: %s.\n", to_msg);
+  printf("bytes written: %d\n", nr);
+
   return 0;
 }
 
 int main() {
-  int parent_to_child_pipe[2];
+  int parent_to_child_pipe[2], child_to_parent_pipe[2];
+
 
   // Create the pipe
   if (pipe(parent_to_child_pipe) == -1) {
+    perror("could not create pipe");
+    exit(1);
+  }
+
+  if (pipe(child_to_parent_pipe) == -1) {
     perror("could not create pipe");
     exit(1);
   }
@@ -51,12 +87,13 @@ int main() {
     // Child process...
     // First we close the write end of the pipe.
     close(parent_to_child_pipe[1]);
-    exit(child_task(parent_to_child_pipe[0]));
+    close(child_to_parent_pipe[0]);
+    exit(child_task(parent_to_child_pipe[READ], child_to_parent_pipe[WRITE]));
   } else {
     // Parent process...
     // First, we close the read end of the pipe.
     close(parent_to_child_pipe[0]);
-    exit(parent_task(parent_to_child_pipe[1]));
+    exit(parent_task(child_to_parent_pipe[READ], parent_to_child_pipe[WRITE]));
 
     // Wait on child process to complete
     wait(NULL);
